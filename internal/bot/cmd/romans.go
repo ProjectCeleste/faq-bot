67e54b915ece41caa3e4bafa1c45352e682cf6c3
SystemGlitch <jeremy.la@outlook.fr>
2021-03-14 21:43:14 +0100

    Update romans

--- internal/bot/cmd/romans.go
+++ internal/bot/cmd/romans.go
@@ -48,7 +48,7 @@ func NewRomans() *Romans {
 
 // Answer the romans release date.
 func (c *Romans) Answer(s *discordgo.Session, m *discordgo.MessageCreate) error {
-	status := "Beware the Ides of March! We shall throw open the gates of Rome on **March 15**. See you all in the Eternal City! "
+	status := "We are already marching triumphantly through the streets of the Eternal City of Rome! Roma Invicta! "
 
 	if c.caesarEmoji == nil {
 		emoji, emojiErr := findEmoji(s, m.GuildID, "Crassus")
